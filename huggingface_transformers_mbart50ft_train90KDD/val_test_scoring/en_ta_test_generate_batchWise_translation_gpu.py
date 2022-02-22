0627bc04fd889ea11929013a4fa0181307ce5d49
Aloka Fernando <36888985+aloka-fernando@users.noreply.github.com>
2022-02-22 22:46:25 +0530

    Add files via upload

---
+++ huggingface_transformers_mbart50ft_train90KDD/val_test_scoring/en_ta_test_generate_batchWise_translation_gpu.py
@@ -0,0 +1,71 @@
+import os
+import time
+import torch
+import math
+from transformers import MBartForConditionalGeneration, MBart50TokenizerFast
+
+checkpoint='mbart50-ft-en-ta-run4'
+print('Evaluation Scores for {}'.format(checkpoint))
+
+os.chdir('/userdirs/aloka/nmt_baseline_experiments/mbart50ft_huggingface_api')
+#initialize tokenizer
+tokenizer = MBart50TokenizerFast.from_pretrained(checkpoint, src_lang="en_XX")
+
+
+src_lines=[line.strip() for line in open('data/parallel-27.04.2021-ts.un.si-en-ta.en', 'r', encoding='utf8')]
+print('No of lines in ts set : {}'.format(len(src_lines)))
+
+#parameters
+input_batchSize=32
+input_batches= math.ceil(len(src_lines)/input_batchSize)
+print('No of batches : {}'.format(input_batches))
+
+
+directories=os.listdir('/userdirs/aloka/nmt_baseline_experiments/mbart50ft_huggingface_api/'+checkpoint)
+# 
+#modelLoadingTime=0
+for subDir in directories :
+    
+    hf_trans_lines=[]
+
+    if subDir.find('checkpoint-') != -1: 
+        print(subDir)  
+        fileOut = open('data/parallel-27.04.2021-ts-translated-run4-checkpoint-360000-r2.un.en-ta.ta', 'w', encoding='utf8')
+
+        #startTime=time.time() 
+        model = MBartForConditionalGeneration.from_pretrained(checkpoint+"/"+subDir).to("cuda")  
+        #modelLoadingTime=time.time()
+        #print('Model Loading Time: {:0.2f}s'.format(modelLoadingTime-startTime))
+        
+        
+        src_lines_minibatch=[]
+
+        for i in range(0,input_batches):
+
+            torch.cuda.empty_cache()
+            if i*input_batchSize+input_batchSize < len(src_lines):
+                src_lines_minibatch=src_lines[i*input_batchSize:i*input_batchSize+input_batchSize]
+            else:
+                src_lines_minibatch=src_lines[i*input_batchSize:]                  
+            
+            
+            model_inputs = tokenizer(src_lines_minibatch, padding=True, truncation=True, return_tensors="pt")                
+            #model_inputs = tokenizer(src_lines_minibatch, padding=True, truncation=True, max_length=100, return_tensors="pt")                
+            generated_tokens = model.generate(**model_inputs.to("cuda"), forced_bos_token_id=tokenizer.lang_code_to_id["ta_IN"])                
+            trans_lines=tokenizer.batch_decode(generated_tokens, batch_size=32, skip_special_tokens=True)
+            
+
+            hf_trans_lines=hf_trans_lines+ trans_lines        
+            
+        for line in hf_trans_lines:                
+                fileOut.write('{}\n'.format(line))
+
+        fileOut.close()
+
+        os.system('sacrebleu -tok "none" -s "none" data/parallel-27.04.2021-ts.un.si-en-ta.ta < data/parallel-27.04.2021-ts-translated-run4-checkpoint-360000-r2.un.en-ta.ta ')        
+
+        #endTime=time.time()
+        #print('{} : {:0.2f}min'.format(subDir, (endTime-startTime)/60))        
+           
+
+print('Evaluating Testset complete!')
